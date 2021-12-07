0f234b4c7a4f0cefac123a0b7184c7eb794c1bc2
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-12-07 15:11:52 +0900

    feat : 로딩 바 추가

--- gui.py
+++ gui.py
@@ -14,34 +14,13 @@ import numpy as np
 from PIL import Image, ImageTk
 from matplotlib.backends.backend_tkagg import *
 from matplotlib.figure import Figure
-# from natsort import natsorted
 from pyscreenshot import grab
+from tkinter.ttk import Progressbar
 
-Path('saved_images').mkdir(exist_ok=True)  # 스크린샷 저장할 경로 생성
-CAPTURED_IMAGES_PATH = os.getcwd() / Path("saved_images")
-BBOX_RESULT_PATH = os.getcwd() / Path("yolov5/runs/detect")
+# Path('saved_images').mkdir(exist_ok=True)  # 스크린샷 저장할 경로 생성
+BBOX_RESULT_PATH = os.getcwd() / Path("yolov5_object_size/runs/detect/bbox")
 SEG_RESULT_PATH = os.getcwd() / Path("Pytorch-UNet/output")
-
-def snapshot():
-    # Warning:
-    # 같은 이름으로 저장된다면 실제디렉토리엔 하나만 저장되지만, capture_lb엔 계속 추가된다.
-    filename = time.strftime("%Y-%m-%d-%H-%M-%S") + ".png"
-    dir = "saved_images/" + filename
-
-    target_img = ImageTk.getimage(video10.img)  # capture segmentation field
-    target_img.save(dir)
-    print("Screenshot Saved..", type(target_img))
-    capture_lb.insert(END, filename)
-
-
-# 캡쳐 사진으로 교체
-def popup_saved_image(filename):
-    print(filename)
-    FILE_DIR = CAPTURED_IMAGES_PATH / Path(filename)
-    img = Image.open(FILE_DIR)
-    img = ImageTk.PhotoImage(image=img)
-    video11.img = img
-    video11.configure(image=img)
+SIZE_RESULT_PATH = os.getcwd() / Path("yolov5_object_size/runs/detect/size")
 
 
 def find_directory():
@@ -59,40 +38,87 @@ def find_directory():
 
 
 def load_resize_result(crack_result, video_loc):
-    width = int(crack_result.size[0] )
-    height = int(crack_result.size[1] )
+    width = int(crack_result.size[0])
+    height = int(crack_result.size[1])
     crack_result = crack_result.resize((width, height), Image.ANTIALIAS)
     crack_result = ImageTk.PhotoImage(image=crack_result)
     video_loc.img = crack_result
     video_loc.configure(image=crack_result)
 
+def progress(pb, value):
+    pb['value'] = value
+
 def predict_bbox_yolov5(img_source):
+    pb01.pack(expand=True)
+
+    progress(pb01, 70)
+    pb01.update()
     print("==bbox predict....==")
+
     # 추론한 결과가 BBOX_RESULT_PATH 경로에 저장됨
     os.system(
-        'python ./yolov5/detect.py --weight ./yolov5/runs/train/results/weights/best.pt --img 416 --conf 0.3 --source ' + img_source)
+        'python ./yolov5_object_size/detect.py --weight ./yolov5_object_size/runs/train/results/weights/best.pt --img 416 --conf 0.3 --source ' + img_source)
+    # progress(pb01, 80)
+    # pb01.update()
 
-    # TODO : natsort를 사용할 수 있도록 python<3.6 으로 downgrade 필요
-    # print(natsorted(os.listdir(BBOX_RESULT_PATH)))
-    last_folder = os.listdir(BBOX_RESULT_PATH)[-1]
-    last_img = os.listdir(BBOX_RESULT_PATH / Path(last_folder))[-1]
-    last_result = BBOX_RESULT_PATH / Path(last_folder) / Path(last_img)
-    print("last_result", last_result)
+    last_result = BBOX_RESULT_PATH / Path(img_source.split("/")[-1])
+    print(last_result)
+    # progress(pb01, 90)
+    # pb01.update()
+
     crack_result = Image.open(last_result)
+
     load_resize_result(crack_result, video01)
+    # progress(pb01, 100)
+    # pb01.update()
+    pb01.pack_forget()
 
 
 def predict_seg_unet(img_source):
+    pb10.pack(expand=True)
+    progress(pb10, 70)
+    pb10.update()
     print("==seg predict....==")
-    seg_result_name = img_source.split("/")[-1].split(".")[0]+".png"
-    os.system('python ./Pytorch-UNet/predict.py -m ./Pytorch-UNet/MODEL.pth -i '+img_source+'  -o '+os.path.join(SEG_RESULT_PATH,seg_result_name))
-    print(seg_result_name, os.path.join(SEG_RESULT_PATH,seg_result_name))
-    crack_result = Image.open(os.path.join(SEG_RESULT_PATH,seg_result_name))
+
+    seg_result_name = img_source.split("/")[-1].split(".")[0] + ".png"
+    os.system('python ./Pytorch-UNet/predict.py -m ./Pytorch-UNet/MODEL.pth -i ' + img_source + '  -o ' + os.path.join(
+        SEG_RESULT_PATH, seg_result_name))
+    # progress(pb10, 80)
+    # pb10.update()
+
+    crack_result = Image.open(os.path.join(SEG_RESULT_PATH, seg_result_name))
+    # progress(pb10, 90)
+    # pb10.update()
+
     load_resize_result(crack_result, video10)
+    # progress(pb10, 100)
+    # pb10.update()
+    pb10.pack_forget()
+
+
+def prdict_size_yolov5(img_source):
+    pb11.pack(expand=True)
+    print("==size predict....==")
+    progress(pb11, 70)
+    pb11.update()
+
+    last_result = SIZE_RESULT_PATH / Path(img_source.split("/")[-1])
+    # progress(pb11, 80)
+    # pb11.update()
+
+    crack_result = Image.open(last_result)
+    # progress(pb11, 90)
+    # pb11.update()
+
+    load_resize_result(crack_result, video11)
+    # progress(pb11, 100)
+    # pb11.update()
+    pb11.pack_forget()
 
 def predict_all():
     img_source = find_directory()
-    if img_source !=None:
+    if img_source is not None:
+        print(img_source)
         start1 = time.time()
         predict_bbox_yolov5(img_source)
         end1 = time.time()
@@ -103,111 +129,92 @@ def predict_all():
         end2 = time.time()
         print(f"seg: {end2 - start2:.5f} sec")
 
-        print("total: %.5f"%(end1-start1 + end2 -start2))
+        start3 = time.time()
+        prdict_size_yolov5(img_source)
+        end3 = time.time()
+        print(f"size: {end2 - start2:.5f} sec")
+        print("total: %.5f" % (end1 - start1 + end2 - start2 + end3 - start3))
 
 # -------------tkinter-----------------
 window = Tk()
 
 # 창을 screen 중간에 열기
-w = 1200  # width for the Tk root
-h = 650  # height for the Tk root
+w = window.winfo_screenwidth()  # width for the Tk root
+h = 800  # height for the Tk root
 
 ws = window.winfo_screenwidth()  # width of the screen
 hs = window.winfo_screenheight()  # height of the screen
-
+print(ws, hs)
 x = (ws / 2) - (w / 2)
 y = (hs / 2) - (h / 2)
 
 window.geometry('%dx%d+%d+%d' % (w, h, x, y))
 window.minsize(w, h)
-window.configure(bg="white")
-window.title("캡스톤디자인 NETFLEX팀 경계검출")
+window.configure(bg="#FFFFFF")
+window.title("캡스톤디자인 NETFLEX팀 주제2")
 window.bind('<Escape>', lambda e: window.quit())  # esc로 창 종료
 window.resizable(True, True)
+header_footerFont = Font(family='Tahoma', size=10)
+scaleFont = Font(family='Tahoma', size=12)
 
-# Canvas 3x2
-canvas_frame = Frame(window, width=w, height=h)
-canvas_frame.pack(fill=BOTH, expand=YES)
-canvas_frame.columnconfigure(0, weight=3)
-canvas_frame.columnconfigure(1, weight=3)
-canvas_frame.rowconfigure(0, weight=3)
-canvas_frame.rowconfigure(1, weight=3)
-canvas_frame.rowconfigure(2, weight=1)
+# 상단 업로드 버튼
+header_frame = Frame(window, bg="white")
+header_frame.place(x=0, y=0, width=w, height=30)
 
-# Top layout
-video00 = Label(canvas_frame, bg="lightslategray", borderwidth=2, relief="ridge")
-video01 = Label(canvas_frame, bg="lightslategray", borderwidth=2, relief="ridge")
-video10 = Label(canvas_frame, bg="lightslategray", borderwidth=2, relief="ridge")
-video11 = Label(canvas_frame, bg="lightslategray", borderwidth=2, relief="ridge")
-
-video00.grid(row=0, column=0, sticky=NSEW)
-video01.grid(row=0, column=1, sticky=NSEW)
-video10.grid(row=1, column=0, sticky=NSEW)
-video11.grid(row=1, column=1, sticky=NSEW)
-
-# bottom layout
-scaleFont = Font(family='Tahoma', size=10, weight='bold')
-
-bottom_layout = Frame(canvas_frame, bg="white")
-bottom_layout.grid(row=2, columnspan=2, sticky=NSEW)
-bottom_layout.columnconfigure(0, weight=6)
-bottom_layout.columnconfigure(1, weight=1)
-bottom_layout.rowconfigure(0, weight=1)
-
-# Parameter layout 2x2
-param_layout = Frame(bottom_layout)
-param_layout.grid(row=0, column=0, sticky=NSEW)
-param_layout.rowconfigure(0, weight=1)
-param_layout.rowconfigure(1, weight=3)
-param_layout.columnconfigure(0, weight=1)
-param_layout.columnconfigure(1, weight=7)
-
-# row=0
-blur_frame = Frame(param_layout, bg="white", borderwidth=1, relief=SUNKEN)
-blur_frame.grid(row=0, column=0, sticky=NSEW)
-filter_frame = Frame(param_layout, bg="white", borderwidth=1, relief=SUNKEN)
-filter_frame.grid(row=0, column=1, sticky=NSEW)
-
-upload_img_btn = Button(filter_frame, text="upload image",
+upload_img_btn = Button(header_frame, text="upload image",
                         bg="midnightblue",
                         fg="white",
+                        width="15",
                         font=scaleFont,
                         command=lambda: predict_all())  # find_directory())
 upload_img_btn.pack(side="right")
 
-# Capture layout
-capture_layout = Frame(bottom_layout, bg="white")
-capture_layout.grid(row=0, column=1, rowspan=2, sticky="NSEW")
+# Canvas 3x2
+canvas_frame = Frame(window, bg="green")
+canvas_frame.place(x=0, y=30, width=w, height=h - 60)
 
-capture_btn = Button(capture_layout,
-                     text="Save Image",
-                     font=scaleFont,
-                     fg="white",
-                     bg="#4535AA",
-                     activeforeground="#009888",
-                     borderwidth=3,
-                     command=lambda: snapshot(),
-                     relief="groove")
-capture_btn.pack(pady=3)
+# Footer
+footer_label = Label(window, bg="#B7472A", font=header_footerFont, fg="white", text="Netflex.t of Sejong Univ.")
+footer_label.place(x=0, y=h - 30, width=w, height=30)
 
-scrollbar = Scrollbar(capture_layout)
-scrollbar.pack(side="right", fill="y")
+# Top layout
+frame_tmp00 = Frame(canvas_frame, borderwidth=2, relief="ridge")
+frame_tmp01 = Frame(canvas_frame, borderwidth=2, relief="ridge")
+frame_tmp10 = Frame(canvas_frame, borderwidth=2, relief="ridge")
+frame_tmp11 = Frame(canvas_frame, borderwidth=2, relief="ridge")
 
-capture_lb = Listbox(capture_layout, yscrollcommand=scrollbar.set)
-capture_lb['bg'] = "black"
-capture_lb['fg'] = "lime"
-capture_lb['font'] = scaleFont
-capture_lb.pack(pady=3, fill="x", expand="yes")
-capture_lb.bind("<<ListboxSelect>>", lambda x: popup_saved_image(capture_lb.get(capture_lb.curselection())))
+label00 = Label(master=frame_tmp00, width=int(w / 2), bg="#B7472A", fg="white", text="Original")
+label01 = Label(master=frame_tmp01, width=int(w / 2), bg="#B7472A", fg="white", text="Bounding Box")
+label10 = Label(master=frame_tmp10, width=int(w / 2), bg="#B7472A", fg="white", text="Segmentation")
+label11 = Label(master=frame_tmp11, width=int(w / 2), bg="#B7472A", fg="white", text="Crack size")
 
-for file in os.listdir(CAPTURED_IMAGES_PATH):
-    capture_lb.insert(END, file)
+video00 = Label(master=frame_tmp00, width=int(w / 2))
+video01 = Label(master=frame_tmp01, width=int(w / 2))
+video10 = Label(master=frame_tmp10, width=int(w / 2))
+video11 = Label(master=frame_tmp11, width=int(w / 2))
 
-scrollbar["command"] = capture_lb.yview
+frame_tmp00.place(x=0, y=0, width=int(w / 2), height=370)
+frame_tmp01.place(x=int(w / 2), y=0, width=int(w / 2), height=370)
+frame_tmp10.place(x=0, y=370, width=int(w / 2), height=370)
+frame_tmp11.place(x=int(w / 2), y=370, width=int(w / 2), height=370)
+
+label00.pack(side="top")
+label01.pack(side="top")
+label10.pack(side="top")
+label11.pack(side="top")
+
+video00.pack(side="bottom", fill=BOTH, expand=YES)
+video01.pack(side="bottom", fill=BOTH, expand=YES)
+video10.pack(side="bottom", fill=BOTH, expand=YES)
+video11.pack(side="bottom", fill=BOTH, expand=YES)
+
+pb01 = Progressbar(video01, orient=HORIZONTAL, length=100, mode='determinate')
+pb10 = Progressbar(video10, orient=HORIZONTAL, length=100, mode='determinate')
+pb11 = Progressbar(video11, orient=HORIZONTAL, length=100, mode='determinate')
+
 # -----------------------------------
 
 if __name__ == "__main__":
     print("start")
-    # cam_thread()
     window.mainloop()
     print("end")
